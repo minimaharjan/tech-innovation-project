f8cc9538b4f5ca6e9ddae9e0f567f14845fccedc
Mini Maharjan <minimaharjan22@gmail.com>
2023-10-11 14:38:04 +1100

    added right side menu as per mode selected on mode modal

--- src/components/Body/ModeModal.js
+++ src/components/Body/ModeModal.js
@@ -1,3 +1,7 @@
+import Bayesian from 'components/Menu/Bayesian';
+import Estimation from 'components/Menu/Estimation';
+import Simulation from 'components/Menu/Simulation';
+import { Row, Col } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
@@ -8,13 +12,29 @@ function ModeModal(props) {
       className="modal show"
       style={{ display: 'block', position: 'initial' }}
     >
-      <Modal.Dialog>
+      <Modal.Dialog size='lg'>
         <Modal.Header closeButton>
           <Modal.Title>{title}</Modal.Title>
         </Modal.Header>
 
         <Modal.Body>
-          <p>Modal body text goes here.</p>
+          
+          <Row>
+        <Col xs={12} md={8}>1 of 1</Col>
+        <Col xs={6} md={4}>
+        {(() => {
+        switch (title) {
+          case "Simulation":   return (<Simulation/>);
+          case "Estimation": return (<Estimation/>);
+          case "Bayesian Estimation":  return (<Bayesian/>);
+          default:      return (<Simulation/>);
+        }
+      })()}
+            {/* {title === "Simulation" ?
+        <Simulation/> :[]   
+        } */}
+        </Col>
+      </Row>
         </Modal.Body>
 
         <Modal.Footer>
